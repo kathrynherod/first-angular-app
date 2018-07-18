fbf022c891968d9479bfddd16ca8100616e16858
Herod, Kathryn <codingkat7@gmail.com>
2018-07-18 11:18:54 -0500

    fixed issue with shouldBeUnique validator by moving it outside of the formcontrol array within username

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -9,18 +9,31 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace,
-      UsernameValidators.shouldBeUnique
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3)
-    ])
+    account: new FormGroup({
+      username: new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          UsernameValidators.cannotContainSpace
+        ],
+        UsernameValidators.shouldBeUnique
+      ),
+      password: new FormControl('', Validators.required)
+    })
   });
+
+  login() {
+    // let isValid = authService.login(this.form.value);
+    const isValid = false;
+    if (!isValid) {
+      this.form.setErrors({
+        invalidLogin: true
+      });
+    }
+  }
+
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
